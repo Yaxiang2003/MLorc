80b7f8f1e495fa440da41f282e7fa39b0ca39d26
Yaxiang2003 <142752432+Yaxiang2003@users.noreply.github.com>
2025-03-25 23:14:17 +0800

    Update optim.py

--- optim.py
+++ optim.py
@@ -5,18 +5,31 @@ import torch.nn as nn
 import torch
 
 
+def randomized_svd(A, rank):
 
-def randomized_svd(Y, rank):
+    m, n = A.shape
+    device = A.device
 
-    Q, _ = torch.linalg.qr(Y)
+
+    random_matrix = torch.randn(size=(n, rank), device=device)
+
+    Y = A @ random_matrix.bfloat16()
+
+    Q, _ = torch.linalg.qr(Y.float())
+
+    Q = Q.bfloat16()
 
     B = Q.T @ A
 
-    U_hat, S, V = torch.linalg.svd(B, full_matrices=False)
+    U_hat, S, V = torch.linalg.svd(B.float(), full_matrices=False)
 
-    U = Q @ U_hat
+    U = Q @ U_hat.bfloat16()
 
-    return U[:, :rank], S[:rank], V[:rank, :]
+    S = S.bfloat16()
+
+    V = V.bfloat16()
+
+    return U, S, V
 
 class RSVD_CM_AdamW(Optimizer):
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1):
@@ -74,10 +87,16 @@ class RSVD_CM_AdamW(Optimizer):
 
                 state["step"] += 1
 
-                random_matrix = torch.randn(size=(p.data.shape[1], self.rank+5), device=p.data.device)
-                Y1 = grad @ ((1-beta1) * random_matrix) + beta1 * m_u @ torch.diag(m_s) @ (m_v @ random_matrix)
-                Y2 = (grad * grad) @ ((1-beta2) * random_matrix) + beta2 * sq_u @ torch.diag(sq_s) @ (sq_v @ random_matrix)
+                m=beta1 * m_u @ torch.diag(m_s) @ m_v + (1-beta1) * grad
+                sq=beta2 * sq_u @ torch.diag(sq_s) @ sq_v + (1-beta2) * grad * grad
 
+                m_u, m_s, m_v = randomized_svd(m, self.rank)
+                sq_u, sq_s, sq_v = randomized_svd(sq, self.rank)
+
+
+
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
                 denom = sq.sqrt().add_(group["eps"])
 
                 step_size = group["lr"]
@@ -86,10 +105,8 @@ class RSVD_CM_AdamW(Optimizer):
                     bias_correction2 = 1.0 - beta2 ** state["step"]
                     step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
 
-                p.data.addcdiv_(-step_size, beta1 * m_u @ torch.diag(m_s) @ m_v + (1-beta1) * grad, (beta2 * sq_u @ torch.diag(sq_s) @ sq_v + (1-beta2) * grad * grad).sqrt().add_(group["eps"]))
+                p.data.addcdiv_(-step_size, m, denom)
 
-                m_u, m_s, m_v = randomized_svd(Y1, self.rank)
-                sq_u, sq_s, sq_v = randomized_svd(Y2, self.rank)
 
                  # Just adding the square of the weights to the loss function is *not*
                 # the correct way of using L2 regularization/weight decay with Adam,
@@ -101,14 +118,14 @@ class RSVD_CM_AdamW(Optimizer):
                 # Add weight decay at the end (fixed version)
                 if group["weight_decay"] > 0.0:
                     p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
-                if state['step']%5 == 1:
-                    print(torch.cuda.memory_summary(device=p.data.device, abbreviated=False))
+
         return loss
 
-class RSVD_AdamW(Optimizer):
+class GaLore(Optimizer):
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1, T=100):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         self.rank=rank
+        self.T=T
         super().__init__(params, defaults)
 
 
@@ -150,15 +167,13 @@ class RSVD_AdamW(Optimizer):
 
                 state["step"] += 1
 
-                if(state["step"]%T==1):                
+                if(state["step"]%self.T==1):                
 
                     u, s, v=torch.linalg.svd(grad.float(), full_matrices=False)
                     state["projector"] = u[:, :self.rank].bfloat16()
 
                 Projector = state["projector"]
                 R_=Projector.T @ grad
-
-
 
                 # Decay the first and second moment running average coefficient
                 # In-place operations to update the averages at the same time
@@ -181,9 +196,97 @@ class RSVD_AdamW(Optimizer):
                 if group["weight_decay"] > 0.0:
                     p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
 
+        return loss
+
+class MLorc_AdamW_perlayer(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
+        self.rank=rank
+        for group in self.params_groups:
+            for p in group["params"]:
+                state = self.state[p]
+                state["step"] = 0
+                # Exponential moving average of gradient values
+                state["m_u"] = torch.zeros((p.data.shape[0], self.rank), dtype=p.data.dtype, device=p.data.device)
+                state["m_v"] = torch.zeros((self.rank, p.data.shape[1]), dtype=p.data.dtype, device=p.data.device)
+                state["m_s"] = torch.zeros((self.rank), dtype=p.data.dtype, device=p.data.device)
+                # Exponential moving average of squared gradient values
+                state["sq_u"] = torch.zeros((p.data.shape[0], self.rank), dtype=p.data.dtype, device=p.data.device)
+                state["sq_v"] = torch.zeros((self.rank, p.data.shape[1]), dtype=p.data.dtype, device=p.data.device)
+                state["sq_s"] = torch.zeros((self.rank), dtype=p.data.dtype, device=p.data.device)
+                
+                if p.requires_grad:
+                    p.register_hook(self.step)
+                    
+        super(MLorc_AdamW_perlayer, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.params_groups:
+            for p in group["params"]:
+                if p.requires_grad and p.grad is not None and p.grad.data.dim()==2:
+                    
+                    grad = p.grad.data
+                    p.grad = None
+                    if p.grad.data.is_sparse:
+                        raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
 
+                    state = self.state[p]
+
+                    m_u, m_v, m_s, sq_u, sq_v, sq_s = state["m_u"], state["m_v"], state["m_s"], state["sq_u"], state["sq_v"], state["sq_s"]
+
+                    beta1, beta2 = group["betas"]
+
+                    state["step"] += 1
+
+                    m=beta1 * m_u @ torch.diag(m_s) @ m_v + (1-beta1) * grad
+                    sq=beta2 * sq_u @ torch.diag(sq_s) @ sq_v + (1-beta2) * grad * grad
+
+                    m_u, m_s, m_v = randomized_svd(m, self.rank)
+                    sq_u, sq_s, sq_v = randomized_svd(sq, self.rank)
+
+
+
+                    # Decay the first and second moment running average coefficient
+                    # In-place operations to update the averages at the same time
+                    denom = sq.sqrt().add_(group["eps"])
+
+                    step_size = group["lr"]
+                    if 'correct_bias' in group and group["correct_bias"]:  # No bias correction for Bert
+                        bias_correction1 = 1.0 - beta1 ** state["step"]
+                        bias_correction2 = 1.0 - beta2 ** state["step"]
+                        step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+
+                    p.data.addcdiv_(-step_size, m, denom)
+
+
+                    # Just adding the square of the weights to the loss function is *not*
+                    # the correct way of using L2 regularization/weight decay with Adam,
+                    # since that will interact with the m and v parameters in strange ways.
+                    #
+                    # Instead we want to decay the weights in a manner that doesn't interact
+                    # with the m/v parameters. This is equivalent to adding the square
+                    # of the weights to the loss with plain (non-momentum) SGD.
+                    # Add weight decay at the end (fixed version)
+                    if group["weight_decay"] > 0.0:
+                        p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
 
         return loss
 
-              
