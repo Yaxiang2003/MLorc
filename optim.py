7e06075c042b6accdf474cfa145680c5edb7837c
Yaxiang2003 <142752432+Yaxiang2003@users.noreply.github.com>
2025-03-31 16:26:12 +0800

    Update optim.py

--- optim.py
+++ optim.py
@@ -4,35 +4,27 @@ from torch.optim.optimizer import Optimizer, required
 import torch.nn as nn
 import torch
 
-
 def randomized_svd(A, rank):
 
     m, n = A.shape
     device = A.device
-
-
     random_matrix = torch.randn(size=(n, rank), device=device)
-
-    Y = A @ random_matrix.bfloat16()
-
+    datatype = A.dtype
+    
+    Y = A @ random_matrix.to(datatype)
     Q, _ = torch.linalg.qr(Y.float())
-
-    Q = Q.bfloat16()
-
+    Q = Q.to(datatype)
     B = Q.T @ A
-
     U_hat, S, V = torch.linalg.svd(B.float(), full_matrices=False)
 
-    U = Q @ U_hat.bfloat16()
-
-    S = S.bfloat16()
-
-    V = V.bfloat16()
+    U = Q @ U_hat.to(datatype)
+    S = S.to(datatype)
+    V = V.to(datatype)
 
     return U, S, V
 
-class RSVD_CM_AdamW(Optimizer):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1):
+class MLorc_AdamW(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=4):
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
         if not 0.0 <= betas[0] < 1.0:
@@ -44,6 +36,8 @@ class RSVD_CM_AdamW(Optimizer):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         self.rank=rank
         super().__init__(params, defaults)
+
+
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -60,15 +54,14 @@ class RSVD_CM_AdamW(Optimizer):
                 if p.grad is None:
                     continue
                 grad = p.grad.data
+                p.grad = None
 
-                if p.grad.data.dim() != 2:
+                if grad.dim() != 2:
                     continue
-                if p.grad.data.is_sparse:
+                if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-
+                    
                 state = self.state[p]
-
                 # State initialization
                 if len(state) == 0:
                     state["step"] = 0
@@ -93,8 +86,6 @@ class RSVD_CM_AdamW(Optimizer):
                 m_u, m_s, m_v = randomized_svd(m, self.rank)
                 sq_u, sq_s, sq_v = randomized_svd(sq, self.rank)
 
-
-
                 # Decay the first and second moment running average coefficient
                 # In-place operations to update the averages at the same time
                 denom = sq.sqrt().add_(group["eps"])
@@ -107,8 +98,7 @@ class RSVD_CM_AdamW(Optimizer):
 
                 p.data.addcdiv_(-step_size, m, denom)
 
-
-                 # Just adding the square of the weights to the loss function is *not*
+                # Just adding the square of the weights to the loss function is *not*
                 # the correct way of using L2 regularization/weight decay with Adam,
                 # since that will interact with the m and v parameters in strange ways.
                 #
@@ -121,8 +111,67 @@ class RSVD_CM_AdamW(Optimizer):
 
         return loss
 
+
+class MLorc_Lion(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.95, 0.98), weight_decay=0.05,  rank=4):
+        defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay)
+        self.rank=rank
+        super().__init__(params, defaults)
+
+
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+
+                if grad.dim() != 2:
+                    continue
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+                    
+                state = self.state[p]
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+
+                    state["m_u"] = torch.zeros((p.data.shape[0], self.rank), dtype=p.data.dtype, device=p.data.device)
+                    state["m_v"] = torch.zeros((self.rank, p.data.shape[1]), dtype=p.data.dtype, device=p.data.device)
+                    state["m_s"] = torch.zeros((self.rank), dtype=p.data.dtype, device=p.data.device)
+
+                m_u, m_v, m_s= state["m_u"], state["m_v"], state["m_s"]
+                beta1, beta2 = group["betas"]
+
+                m=m_u @ torch.diag(m_s) @ m_v
+                update=(beta1 * m + (1-beta1) * grad).sign_()
+
+                state["step"] += 1
+                step_size = group["lr"]
+                p.data.add_(update, alpha=-step_size)
+
+                m_=beta2 * m + (1-beta2) * grad
+                m_u, m_s, m_v = randomized_svd(m_, self.rank)
+
+                if group["weight_decay"] > 0.0:
+                    p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
+
+        return loss
+
+
 class GaLore(Optimizer):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1, T=100):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=4, T=100):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         self.rank=rank
         self.T=T
@@ -139,7 +188,7 @@ class GaLore(Optimizer):
         loss = None
         if closure is not None:
             loss = closure()
-        
+
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None:
@@ -150,7 +199,7 @@ class GaLore(Optimizer):
                     continue
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-                
+
 
                 state = self.state[p]
 
@@ -167,7 +216,7 @@ class GaLore(Optimizer):
 
                 state["step"] += 1
 
-                if(state["step"]%self.T==1):                
+                if(state["step"]%self.T==1):
 
                     u, s, v=torch.linalg.svd(grad.float(), full_matrices=False)
                     state["projector"] = u[:, :self.rank].bfloat16()
@@ -197,108 +246,5 @@ class GaLore(Optimizer):
                     p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
 
         return loss
-
-class MLorc_AdamW_perlayer(Optimizer):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.01, correct_bias=True, rank=1):
-        if lr < 0.0:
-            raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
-        if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
-        if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
-        self.rank=rank
-        super().__init__(params, defaults)
-
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-
-                if p.grad.data.dim() != 2:
-                    continue
-                if p.grad.data.is_sparse:
-                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-                
-                    
-                def grad_hook(grad, param=p, p_state=state):
-                    if 'correct_bias' in group and group["correct_bias"]:
-                        self._update_param(param, grad, beta=group["betas"], lr=group["lr"], eps=group["eps"], weight_decay=group["weight_decay"], bias_correction=True)
-                    else:
-                        self._update_param(param, grad, beta=group["betas"], lr=group["lr"], eps=group["eps"], weight_decay=group["weight_decay"], bias_correction=False)
-                    # 清空梯度以释放内存
-                    param.grad = None
-                    return grad  # 返回原始梯度以避免修改计算图
-
-                p.grad.register_hook(grad_hook)
-
-         return loss
-        
-    def _update_param(self, param, grad, beta, lr, eps, weight_decay, bias_correction):
-        
-        state = self.state[param]
-        # State initialization
-        if len(state) == 0:
-            state["step"] = 0
-            # Exponential moving average of gradient values
-            state["m_u"] = torch.zeros((param.data.shape[0], self.rank), dtype=param.data.dtype, device=param.data.device)
-            state["m_v"] = torch.zeros((self.rank, param.data.shape[1]), dtype=param.data.dtype, device=param.data.device)
-            state["m_s"] = torch.zeros((self.rank), dtype=param.data.dtype, device=param.data.device)
-            # Exponential moving average of squared gradient values
-            state["sq_u"] = torch.zeros((param.data.shape[0], self.rank), dtype=param.data.dtype, device=param.data.device)
-            state["sq_v"] = torch.zeros((self.rank, param.data.shape[1]), dtype=param.data.dtype, device=param.data.device)
-            state["sq_s"] = torch.zeros((self.rank), dtype=param.data.dtype, device=param.data.device)
-
-        m_u, m_v, m_s, sq_u, sq_v, sq_s = state["m_u"], state["m_v"], state["m_s"], state["sq_u"], state["sq_v"], state["sq_s"]
-
-        beta1, beta2 = beta
-
-        state["step"] += 1
-
-        m=beta1 * m_u @ torch.diag(m_s) @ m_v + (1-beta1) * grad
-        sq=beta2 * sq_u @ torch.diag(sq_s) @ sq_v + (1-beta2) * grad * grad
-
-        m_u, m_s, m_v = randomized_svd(m, self.rank)
-        sq_u, sq_s, sq_v = randomized_svd(sq, self.rank)
-
-
-
-        # Decay the first and second moment running average coefficient
-        # In-place operations to update the averages at the same time
-        denom = sq.sqrt().add_(eps)
-
-        step_size = lr
-        if bias_correction==True:  # No bias correction for Bert
-            bias_correction1 = 1.0 - beta1 ** state["step"]
-            bias_correction2 = 1.0 - beta2 ** state["step"]
-            step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
-
-        param.data.addcdiv_(-step_size, m, denom)
-
-
-                 # Just adding the square of the weights to the loss function is *not*
-                # the correct way of using L2 regularization/weight decay with Adam,
-                # since that will interact with the m and v parameters in strange ways.
-                #
-                # Instead we want to decay the weights in a manner that doesn't interact
-                # with the m/v parameters. This is equivalent to adding the square
-                # of the weights to the loss with plain (non-momentum) SGD.
-                # Add weight decay at the end (fixed version)
-        if group["weight_decay"] > 0.0:
-            param.data.add_(param.data, alpha=-lr * weight_decay)
 
        
