c0704b2d07365f344fc0c9c3c5f102d523f44356
Yaxiang2003 <142752432+Yaxiang2003@users.noreply.github.com>
2025-04-05 16:11:40 +0800

    Update eval_code.py

--- eval_code.py
+++ eval_code.py
@@ -108,8 +108,11 @@ def main():
     print("Environment initialized successfully!Number of gpu is:", torch.cuda.device_count())
 
     # Step 1: load model
-    model_name = "meta-llama/Llama-2-7b-chat-hf"
-    model = transformers.LlamaForCausalLM.from_pretrained(model_name, max_length=1024, attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16, device_map={"": int(os.environ.get("LOCAL_RANK") or 0)}, use_auth_token=True)
+    if config["optimizer"] == "None":
+        model_name = "meta-llama/Llama-2-7b-chat-hf"
+        model = transformers.LlamaForCausalLM.from_pretrained(model_name, max_length=1024, attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16, device_map={"": int(os.environ.get("LOCAL_RANK") or 0)}, use_auth_token=True)
+    else:
+        model = transformers.LlamaForCausalLM.from_pretrained(f'./logs/transformers/llama-2-7b/code/optimizer_{config["optimizer"]}/lr_{config["learning_rate"]}', max_length=1024, attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16, device_map={"": int(os.environ.get("LOCAL_RANK") or 0)}, use_auth_token=True)
     model.config.use_cache = True
     model.gradient_checkpointing_disable()
     tokenizer = transformers.LlamaTokenizer.from_pretrained(model_name, padding_side="left")
